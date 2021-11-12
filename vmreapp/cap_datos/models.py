c1d85fcbbfc43f9098fa7c82ef47f17c79f6312e
José Eduardo Guerrero Martínez <39276193+eltigueeere@users.noreply.github.com>
2021-11-12 14:18:50 -0600

    frist save

--- vmreapp/cap_datos/models.py
+++ vmreapp/cap_datos/models.py
@@ -1,3 +1,10 @@
-from django.db import models
 
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
 # Create your models here.
+class votos(models.Model):
+    partido = models.CharField(max_length=20)
+    voto_postal = models.IntegerField(max_length=33)
+    voto_internet = models.IntegerField(max_length=33)
--- vmreapp/cap_datos/views.py
+++ vmreapp/cap_datos/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from cap_datos import models
 
 
 def cap_datos(request):
+    
+    if request.method == "POST":
+        print(request.POST)
+        print("##########")
+        print(request.POST.get("pan_postal", None))
+        
+        partido = "PAN"
+        voto_postal = request.POST.get("pan_postal", None)
+        voto_internet = request.POST.get("pan_internet", None)
+        twz = models.votos.objects.create(partido = partido, voto_postal=voto_postal, voto_internet=voto_internet)
+        twz.save()
+    
     return render(request, "cap_datos/cap_datos.html")
